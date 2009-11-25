442f1f64f971a30c8806cb9f69c6cc1f20fec06a
rk <thewickedflea@gmail.com>
2009-11-25 15:30:40 -0500

    fixed the require statement in the filter

--- lib/less_filter.rb
+++ lib/less_filter.rb
@@ -1,7 +1,8 @@
+require 'less'
+
 class LessFilter < TextFilter
   description_file File.dirname(__FILE__) + '/description.html'
   def filter(text)
-    require 'less' unless defined?(Less)
     Less.parse(text)
   end
 end
